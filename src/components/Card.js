9063ad403bb446d8dfc4fe48596e69da35958c52
Yahya-Achrane <yahya.achrane12@gmail.com>
2020-07-22 13:50:13 +0100

    fixing some issues

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ name, email, id }) => {
 	return (
-     <div className='bg-light-blue dib br3 pa1 ma1 grow bw2 shadow-5'> 
+     <div className='bg-light-blue dib br2 pa1 ma1 grow bw2 shadow-5'> 
        <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
        <div>
           <h2>{name}</h2>
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const SearchBox = ({ searchfield, searchChange }) => {
 	return (
-	 <div classname='pa2'>	
+	 <div className='pa2'>	
 		<input
-		 className='pa3 ba b--grenn bg-lightest-blue'
+		 className='pa3 ba b--green bg-lightest-blue'
 		 type='search' 
 		 placeholder = 'search robots'
 		 onChange={searchChange}
